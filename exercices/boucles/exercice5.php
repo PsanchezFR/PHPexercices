62465a580095228a0fb7ab03d4fa7f953fa0489e
cpt <p.sanchez.fr@gmail.com>
2018-02-13 10:46:40 +0100

    Exercices 5 and 6 completed.

--- exercices/boucles/exercice5.php
+++ exercices/boucles/exercice5.php
@@ -3,3 +3,11 @@
 /**
 * Creer un script qui affiche les tables de multiplications des nombres de 1 a 5
 */
+$result = 0;
+for ($i=1; $i < 6 ; $i++) { 
+	for ($j=1; $j < 11; $j++) { 
+		$result = $i * $j;
+		echo "$i x $j = $result <br />";
+	}
+}
+?>
--- exercices/boucles/exercice6.php
+++ exercices/boucles/exercice6.php
@@ -4,3 +4,16 @@
  * Ecrire un script qui calcul la somme des entiers de 1 a 20
  * et afficher le resultat
  */
+function intSum1to20(){
+	$result = 0;
+	for ($i=1; $i <= 20 ; $i++) { 
+		$string .= "$i + ";
+		$result += $i;
+	}
+	  trim($string, "+ ");
+	  $string .= " = $result";
+	  return $string;
+}
+
+echo intSum1to20();
+?>
