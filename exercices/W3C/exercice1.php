11682cb2867a280ce5d2df306dbb909eef3fa57f
cpt <p.sanchez.fr@gmail.com>
2018-02-13 11:17:22 +0100

    Completed 2 functions in W3C.

--- exercices/W3C/exercice1.php
+++ exercices/W3C/exercice1.php
@@ -1,30 +1,40 @@
-<!-- 
+<!--  
 Consigne :
 
 1. Write a function to calculate the factorial of a number (a non-negative integer). The function accepts the number as an argument. 
 
 2. Write a function to check a number is prime or not. 
 Note: A prime number (or a prime) is a natural number greater than 1 that has no positive divisors other than 1 and itself.
--->
+ -->
 
 <?php 
-function factorial(x){
-	$result = 0;
-	for ($i=0; $i < x; $i++) { 
-		x *= $result;
-	}
-	return $result;
-}
-
-function testPrime(x){
-	for ($i=0; $i < x ; $i++) { 
-		if($i != 1 && $i != x){
-			if(x % $i == 0){
-				return false;
+	function testPrime($x){
+		for ($i=1; $i < $x ; $i++) { 
+			if($i !== 1){
+				if($x % $i == 0){
+					return 0;
+				}
 			}
 		}
-		return true;
+		return 1;
 	}
-}
+
+	function factorial($x){
+		$result = 1;
+
+		if($x == 0)
+		{
+			return 1;
+		}
+
+		for ($i=1; $i <= $x; $i++) 
+		{ 
+			$result *= $i;
+		}
+		return "Le résultat de $x est $result <br />";
+	}
+
+	echo factorial(7);
+	echo testPrime(4);
 ?>
 
