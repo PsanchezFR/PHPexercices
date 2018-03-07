bc43f7faba621f1f1a1145643aed033ec76cf17c
cpt <p.sanchez.fr@gmail.com>
2018-03-07 13:47:31 +0100

    finished minimum for POO exercice 3

---
+++ exercices.old/poo/exercice3/exercice.php
@@ -0,0 +1,98 @@
+<style>
+	body{
+		display: flex;
+		flex-flow: row;
+		background: lightblue;
+	}
+	h1{
+		align-self: center;
+	}
+	form{
+		display: flex;
+		background:silver;
+		margin: 5px;
+		flex-flow:column;
+		flex-wrap: nowrap;
+		justify-content:center;
+		align-content: center;
+		height: 300px;
+		width: 300px;
+		border: 2px solid black;
+		border-radius: 10%;
+	}
+	form div{
+		display: flex;
+		flex-flow: row;
+		align-self: center;
+		align-content: space-around;
+	}
+	form input{
+		text-align: center;
+	}
+	img{
+		height: 200px;
+		width: 200px;
+		align-self: center;
+		margin: 5px;
+		border: 1px solid gray;
+		border-radius: 5%;
+	}
+</style>
+
+
+<?PHP
+//INITIALIZATION SCRIPT ====================
+// 
+//--- errors detection type
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+//--- includes
+include("cart.php");
+include("product.php");
+//--- session
+session_start();
+
+// MAIN SCRIPT ====================
+//
+	
+	//CREATION OF OBJECTS IN SESSION
+	if(empty($_SESSION['spawned'])){
+		$_SESSION['spawned'] = true;
+		$_SESSION['cart'] = new cart();
+		$_SESSION['oeuf'] = new product("oeuf", 1, "http://www.bioalaune.com/img/article/thumb/900x500/10368-oeufs.jpg", 0, $_SESSION['cart']);
+		$_SESSION['chocolat'] = new product("chocolat", 3, "http://www.patissermalin.fr/img/cms/choc.jpg", 10, $_SESSION['cart']);
+	}
+	//DETECTION OF INPUTS		
+	//$_POST -- action, product, number are the values to use here
+	if(isset($_POST['action'])){
+		//test the kind of action
+		switch ($_POST['action']){
+			//if button delete is pressed
+			case 'delete':			
+				if(ctype_digit($_POST['number'])){
+					$value = strval($_POST['number']);
+					$_SESSION[$_POST['name']]->removeFromCart(false,$value);
+				}
+				break;
+			//if button add is pressed
+			case 'add':			
+				if(ctype_digit($_POST['number'])){
+					$value = strval($_POST['number']);
+					$_SESSION[$_POST['name']]->addToCart($value);
+				}
+				break;
+		}
+		echo $_SESSION['oeuf']->initializeInterface();
+		echo $_SESSION['chocolat']->initializeInterface();
+		echo $_SESSION['cart']->update();
+	}
+
+?>
+
+
+<html> 
+<body>
+	
+</body>
+</html>
---
+++ exercices.old/poo/exercice3/product.php
@@ -0,0 +1,157 @@
+<?PHP //INTERFACES -- ONLY FOR MINIMUM FUNCTIONS AND CONSTANTS ====================
+	interface productInterface
+	{
+			public function addToCart($number);
+			public function removeFromCart($all, $number);
+	}
+
+//CLASS FOR THE PRODUCT ====================
+//
+
+	class product implements productInterface {
+		public function __construct($name, $price, $image, $number, $cart){
+			if(is_string($name)){
+				$this->name = $name;
+			}
+			if(is_float($price)){
+				$this->price = $price;
+			}
+			if(is_string($image)){
+				$this->image = $image;
+			}
+			if($this->validateInteger($number, "__construct()")){
+				$this->currentNumber = $number;
+			}
+			if($this->validateCart($cart, "__construct()")){
+				$this->cart = $cart;
+				$this->initializeInCart();
+				$this->initializeInterface();
+			}
+		}
+
+		//METHODS ******
+		//
+
+		// CALLED IN CONSTRUCTOR ---------------
+
+		// create the corresponding entry in the cart array of products.
+		private function initializeInCart(){
+					if($this->currentNumber > 0){
+						$cart = $this->cart;
+						$cart->addToProductList($this);
+					}
+		}
+
+		//create interface to add and remove objects from the cart
+		public function initializeInterface(){
+				echo "<article class='$this->name'>";
+					echo "<form action='?' method='post'>";
+						echo "<h1>$this->name</h1>";
+						echo "<img src='$this->image'>";
+						echo "<input class='number' type='text' name='number' value=''></input>";
+						echo "<div>";
+							echo "<input class='add' type='submit' name='action' value='add'></input>";
+							echo "<input class='remove' type='submit' name='action' value='delete'></input>";
+							echo "<input class='hidden' type='hidden' name='name' value='$this->name'></input>";
+						echo "</div>";
+					echo "</form >";
+				echo "</article>";
+		}
+
+
+		//GETTERS---------------
+		public function getName(){
+			return $this->name;
+		}
+
+		public function getNumber(){
+			return $this->currentNumber;
+		}
+
+		//SETTERS---------------
+
+		//set the $number of products
+		private function setNumber($number){
+			$cart = $this->cart;
+			$this->currentNumber = $number;
+
+		}
+
+		//MANIPULATION OF CART---------------
+
+		//addToCart can add a $number of objects from the cart object. 
+		public function addToCart($number){
+					//create local references to variables
+					$testResult = $this->validateInteger($number, "addToCart");
+					$cart = $this->cart;
+					//check if $number is valid	
+					if($testResult){
+						$result = $number + $this->currentNumber; //catch the result of adding current number of product and input
+						
+						//different actions if under or over 0
+						if($this->currentNumber > 0){
+							$this->setNumber($result);		//set the new variable
+							$cart->changeNumberOfProduct($this);
+						}
+						else{
+							$this->setNumber($number);		//set the new variable
+							$cart->addToProductList($this);
+							$cart->changeNumberOfProduct($this);
+						}
+					}
+		}
+
+		//removeFromCart can remove $all or a $number of objects from the cart object. 
+		public function removeFromCart($all, $number){
+				//create local references to variables
+				$testResult = $this->validateInteger($number, "removeFromCart");
+				$cart = $this->cart;
+
+				//check the parameter $all
+				if(is_bool($all) && $all && $testResult){
+					$cart->removeFromProductList($this);		//remove product from productList
+				}
+				//check if $number is valid	
+				if($testResult){														
+					$number *= -1;								//set $number to a negative value
+					$result = $number + $this->currentNumber; //catch the result of adding current number of product and input
+
+					//different actions if under or over 0
+					if($this->currentNumber > 0 && $result > 0){
+						$this->setNumber($result);		//set the new variable
+						$cart->changeNumberOfProduct($this);
+					}
+					else{
+						$this->setNumber(0);
+						$cart->changeNumberOfProduct($this);
+						$cart->removeFromProductList($this);
+					}
+				}
+		}
+
+		//TESTS---------------
+
+		//validateParameters should be used for test purpose before modifying the product list
+		//$functionName is used for more specific errors.
+		public function validateInteger($number, $functionName){
+			//is $number a positive integer ?
+			if(!is_int($number) && !$number > 0){
+				trigger_error("$functionName -- " . '$number should be an int.');
+				return false;
+			}
+			// return the parameters if valid
+			return true;
+		}
+
+		//validateParameters should be used for test purpose when assigning a new cart
+		//$functionName is used for more specific errors.
+		public function validateCart($cart, $functionName){
+			if(is_a($cart, "cart")){
+				return true;	
+			}
+			trigger_error("$functionName -- " . '$cart is of invalid class ' . get_class($cart));
+				return false;
+		}
+
+	}
+?>
