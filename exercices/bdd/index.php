2e7bef9f9fc03a91edb95894867cdda32e28bdf5
cpt <p.sanchez.fr@gmail.com>
2018-04-04 12:57:35 +0200

    first commit bdd

---
+++ exercices/bdd/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+        <meta charset="utf-8" />
+        <title>Placement aléatoire</title>
+    </head>
+
+    <body>
+
+        <?php include("connexion.php"); ?>
+
+        <?php include("car.php"); ?>
+
+        <?php include("user.php"); ?>
+
+        <?php include("user_car.php"); ?>
+    </body>
+
+</html>
